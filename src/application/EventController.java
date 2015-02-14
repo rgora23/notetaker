8d21188098ab1fde334d07dbb45d78b7d5f13c3c
Brian Maxwell <brianmaxwell1@gmail.com>
2015-02-13 23:30:05 -0500

    connect controller to main application for view transitions

--- src/application/EventController.java
+++ src/application/EventController.java
@@ -1,15 +1,32 @@
 package application;
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 public class EventController {
-	
-		@FXML private Label noteTaker_text;
-		@FXML protected void ChangeText(ActionEvent event) {
-			  noteTaker_text.setText("NoteTaker");
-			  
-			  
-		}
-	
-	
+	private Main main;
+
+	@FXML private Label noteTaker_text;
+	@FXML private Parent root;
+	@FXML protected void ChangeText(ActionEvent event) throws IOException {
+		noteTaker_text.setText("NoteTaker");
+		// Check db for correct un/pw
+		Stage stage = (Stage) root.getScene().getWindow();
+
+		boolean loginSuccessful = true;
+		getMain().loginSuccessWindow(root);
+
+	}
+
+	public Main getMain() {
+		return main;
+	}
+	public void setMain(Main main) {
+		this.main = main;
+	}
+
+
 }
